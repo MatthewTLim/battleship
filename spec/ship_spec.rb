ab00ada635faaec21dde6969a5f307c5e48f1e3d
Matthew Lim <matlim200@gmail.com>
2023-04-06 17:36:24 -0600

    Feature: add #hit: it sinks when health hits zero test.

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -29,4 +29,17 @@ RSpec.describe Ship do
       cruiser.hit
       expect(cruiser.health).to eq(2)
     end
+
+    it "it sinks when health hits zero" do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.health).to eq(3)
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+      cruiser.hit
+      expect(cruiser.health).to eq(1)
+      cruiser.hit
+      expect(cruiser.health).to eq(0)
+      expect(cruiser.sunk?).to eq(false)
+    end
+  end
 end
