33b669271b9f2d223346cc86cc6133d0bf6dea34
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-06 18:32:50 -0600

    feature: add #empty?

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,13 +1,15 @@
 class Cell
 
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @is_empty = true
+  end
 
-    
-
+  def empty?
+    @is_empty
   end
 
 
