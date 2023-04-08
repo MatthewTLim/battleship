3e36e752406a0db272c7ddac2a154ee99a18abf1
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-08 12:08:28 -0600

    fix: refactor #render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,14 +5,11 @@ class Cell
     @coordinate = coordinate
     @ship = nil
     @is_fired_upon = false
+    @display = "."
   end
 
   def empty?
-    if @ship.nil?
-      true
-    else
-      false
-    end
+    @ship.nil?
   end
 
   def place_ship(ship) 
@@ -20,38 +17,32 @@ class Cell
   end
 
   def fired_upon?
-    @is_fired_upon
+    @display == "H" || @display == "M"
   end
 
   def fire_upon
-    return false if @is_fired_upon == true
+    return "Please enter new coordinates" if fired_upon?
     if @ship = ship
-      @is_fired_upon = true
+      @display = "H"
       @ship.hit
     else
-      @is_fired_upon = true
+      @display = "M"
     end
   end
 
   def render(value=false)
-   if @is_fired_upon == true && @ship == nil
-      "M"
-   elsif @is_fired_upon == false && @ship == ship && value == true
-      "S"
-   elsif @is_fired_upon == true && @ship == ship
-    # require 'pry'; binding.pry
-      "H"
-   elsif @is_fired_upon == false && ship && @ship.health == 0 
-      "X"
-   else
-      "."
+    if value == true
+      if @ship == ship && fired_upon? == false
+        @display = "S"
+      end
+    else
+      if @display == "H" && @ship.sunk?
+        @display = "X"
+      end
+      @display
     end
+
   end
 end
-  # not been fired upon and contains a ship
-  # if the cell has been fired upon and it contains a ship (the shot was a hit).
-  # if the cell has been fired upon and its ship has been sunk.
-  # def render(true)
 
-  # end
 
