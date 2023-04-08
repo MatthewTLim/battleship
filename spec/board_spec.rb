2f2c82a2fba0b15e223d6ad974a19bfba7ae3467
Matthew Lim <matlim200@gmail.com>
2023-04-08 13:21:19 -0600

    Feature: add #valid_coordinate  #valid_placement? methods and tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,9 +13,51 @@ RSpec.describe Board do
     it "has readable attributes" do
       board = Board.new
 
-      expect(board.cells).to be_a(Hash)
+      expect(board.cells.length).to eq(16)
+      expect(board.cells).to include("A1", "D4")
+      expect(board.cells["A1"]).to be_an_instance_of(Cell)
+    end
+  end
+
+  describe "#valid_placement?(ship, coordinates)" do
+    it "can validate coordinate" do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A1")).to be(true)
+      expect(board.valid_coordinate?("D4")).to be(true)
+      expect(board.valid_coordinate?("A5")).to be(false)
+      expect(board.valid_coordinate?("E1")).to be(false)
+      expect(board.valid_coordinate?("A22")).to be(false)
+    end
+  end
+
+  describe "#valid_placement?" do
+    before(:each) do 
+      @board = Board.new
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
+    end
+    
+    it "returns false if number of coordinates is not the same as the ship length" do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be(false)
     end
 
-    
+    it "returns false if coordinates are not consecutive" do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be(false)
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
+    end
+
+    it "returns false if coordinates are diagonal" do
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be(false)
+    end
+
+    it "returns true if placement is valid" do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be(true)
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be(true)
+    end
   end
 end
