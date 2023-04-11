c9727e21671628cca93aa7867560ff453456d58b
Matthew Lim <matlim200@gmail.com>
2023-04-11 14:38:21 -0600

    Feature: add #place_ship method.

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_reader :ships, :health, :player_board
+  attr_reader :ships, :player_board
   def initialize
     @ships = []
     @player_board = Board.new
@@ -15,4 +15,15 @@ class Player
   def add_ship(ship)
     @ships << ship
   end
+
+  def place_ship(ship, coordinates) 
+    # @ships.each do |ship|
+    # until ships == []
+    @player_board.place(ship, coordinates) 
+    # require 'pry'; binding.pry
+
+    # end
+  end
+
+  # place(ship, coordinates) - use this method to place ships for player
 end
