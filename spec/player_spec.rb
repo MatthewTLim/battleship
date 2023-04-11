b279ccdd8a81068aec15ae8d1f64b9ebafae1879
Matthew Lim <matlim200@gmail.com>
2023-04-11 14:37:55 -0600

    Feature: add #place_ship method test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Player do
   describe "#initialize" do
     it "exists" do 
     player1 = Player.new
+
     expect(player1).to be_an(Player)
     end
 
@@ -15,11 +16,48 @@ RSpec.describe Player do
       player1 = Player.new
       cruiser = Ship.new("Cruiser", 3)
       submarine = Ship.new("Submarine", 2)
+
+      expect(player1.ships).to eq([])
+      expect(player1.player_board).to be_an(Board)
+    end 
+  end
+
+  describe "#add_ship" do
+    it "can add a ship" do
+      player1 = Player.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
       expect(player1.ships).to eq([])
       player1.add_ship(cruiser)
       player1.add_ship(submarine)
+      expect(player1.ships).to eq([cruiser, submarine])
+    end
+  end
+
+  describe "#health" do
+    it "has health" do
+      player1 = Player.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      player1.add_ship(cruiser)
+      player1.add_ship(submarine)
+
       expect(player1.health).to eq(5)
-      expect(player1.player_board).to be_an(Board)
+    end 
+  end
+
+  describe "#place_ship" do
+    it "can place a ship" do
+      player1 = Player.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      
+      player1.add_ship(cruiser)
+      player1.add_ship(submarine)
+      player1.place_ship(cruiser, ["A1", "A2", "A3"])
+      expect(player1.player_board.cells["A1"].ship).to be_a(Ship)
     end
   end
 end
