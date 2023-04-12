8df19665bb44360c0f5c3d36711d31ebf599b09e
Matthew Lim <matlim200@gmail.com>
2023-04-12 15:51:16 -0600

    Feature: add #place_ship test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,4 +25,19 @@ RSpec.describe Game do
       expect(game.cpu_player).to eq(cpu_player1)
     end
   end
+
+  describe "#place_ships" do
+    it "can place_ships on board" do
+      player1 = Player.new
+      cpu_player1 = Player.new
+      game = Game.new(player1, cpu_player1)
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(game.player_board).to be_a(Board)
+      game.place_ship(cruiser, ["A1", "A2", "A3"])
+      expect(game.player_board.cells["A1"].ship.nil?).to be(false)
+      expect(game.player_board.cells["A2"].ship.nil?).to be(false)
+      expect(game.player_board.cells["A3Z"].ship.nil?).to be(false)
+    end
+  end
 end
