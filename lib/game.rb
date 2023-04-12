5b713ae45139ae4c210c186921f08bbdb5da501f
Matthew Lim <matlim200@gmail.com>
2023-04-12 15:50:47 -0600

    Feature add: #place_ship method

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,17 @@
 class Game
   attr_reader   :player, 
-                :cpu_player
+                :cpu_player,
+                :player_board,
+                :cpu_board
+
 
   def initialize(player, cpu_player)
     @player = player
     @cpu_player = cpu_player
     @player_ships = {cruiser: Ship.new("Cruiser", 3), submarine: Ship.new("Submarine", 2)}
     @cpu_ships = {cruiser: Ship.new("Cruiser", 3), submarine: Ship.new("Submarine", 2)}
+    @player_board = Board.new
+    @cpu_board = Board.new
   end
 
   # def ships
@@ -14,7 +19,7 @@ class Game
   #   Ship.new("Submarine", 2)
   # end
 
-  def main_menu
+  def setup 
   puts "Welcome to BATTLESHIP Enter p to play. Enter q to quit."
     data = gets.chomp.downcase
     if data == "p"  
@@ -26,4 +31,9 @@ class Game
       main_menu
     end
   end
+
+  def place_ship(ship, coordinates)
+    @player_board.place(ship, coordinates)
+  end
+
 end
